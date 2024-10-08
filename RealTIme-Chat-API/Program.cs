6e9f9a0ba377042f2a6ac5c71bb65c36b11ce311
Viktor Georgiev <71330381+viktor4ou@users.noreply.github.com>
2024-10-08 23:46:28 +0300

    -Add route for SignalR hub -Add SignalR service

--- RealTIme-Chat-API/Program.cs
+++ RealTIme-Chat-API/Program.cs
@@ -8,7 +8,7 @@ namespace RealTIme_Chat_API
             var builder = WebApplication.CreateBuilder(args);
 
             // Add services to the container.
-
+            builder.Services.AddSignalR();
             builder.Services.AddControllers();
             // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
             builder.Services.AddEndpointsApiExplorer();
@@ -29,7 +29,7 @@ namespace RealTIme_Chat_API
 
 
             app.MapControllers();
-
+            app.MapHub<SignalRConnectionHub>("/connect");
             app.Run();
         }
     }
