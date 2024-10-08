8f7673dd85aa14509d1eb4dab264a119959592c9
Viktor Georgiev <71330381+viktor4ou@users.noreply.github.com>
2024-10-08 23:45:55 +0300

    -Create SignalR Hub

---
+++ RealTime-Chat/Hubs/SignalRConnectionHub.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.SignalR;
+
+namespace RealTime_Chat.Hubs
+{
+    public class SignalRConnectionHub : Hub
+    {
+        public override Task OnConnectedAsync()
+        {
+            return base.OnConnectedAsync();
+        }
+        public override Task OnDisconnectedAsync(Exception? exception)
+        {
+            return base.OnDisconnectedAsync(exception);
+        }
+    }
+}
